d4b02f1dfbadf35870de36f9850d95561b4c175b
newgede <gedekhrisnapertama18@gmail.com>
2025-03-21 12:04:21 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint      `gorm:"column:id;primaryKey"`
+	Name      string    `gorm:"column:name"`
+	Email     string    `gorm:"column:email"`
+	Age       string    `gorm:"column:age"`
+	CreatedAt time.Time `gorm:"column:createdAt"`
+	UpdatedAt time.Time `gorm:"column:updatedAt"`
+}
+
+func main() {
+	dsn := "root:@tcp(localhost:3306)/openapi?charset=utf8mb4&parseTime=True&Loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("failed to connect database")
+	}
+
+	router := gin.Default()
+
+	router.GET("/users", func(c *gin.Context) {
+		var users []User
+		db.Find(&users)
+		c.JSON(http.StatusOK, gin.H{"data": users})
+	})
+
+	router.Run(":3000")
+
+}
